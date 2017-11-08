898fdc61f11e54c56579ec84bcc72486a7958ff6
HuyHQ <hqhcodev@gmail.com>
2017-11-08 15:38:28 +0700

    Update index

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import { AppRegistry } from 'react-native';
-import App from './example/App';
+import RNLive from "./lib/RNLive";
 
-AppRegistry.registerComponent('RNLive', () => App);
+export default RNLive;
